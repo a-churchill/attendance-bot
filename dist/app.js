f6ae58be3e3b22e50ecb6593487382e1c8779489
Andrew Churchill <adchurch@mit.edu>
2020-02-01 20:31:20 -0500

    app update

--- dist/app.js
+++ dist/app.js
@@ -8,5 +8,9 @@ if (port == null || port == "") {
 }
 app.listen(port, function () { return console.log("Listening on port " + port); });
 app.get("/", function (req, res) {
-    res.status(200).json("Success");
+    res.status(200).send("This is just a Slack app. Nothing here!");
+});
+app.post("/slash", function (req, res) {
+    console.log("Request body: ", JSON.stringify(req.query));
+    res.status(200).send("Got the slash command!");
 });
