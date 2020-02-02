3a79b731ae41f9e9eeee89c7441bda97c02be2bf
Andrew Churchill <adchurch@mit.edu>
2020-02-01 20:31:20 -0500

    app update

--- src/Heroku/src/app.ts
+++ src/Heroku/src/app.ts
@@ -1,5 +1,6 @@
 import { Express, Request, Response } from "express";
 import express = require("express");
+import bodyParser = require("body-parser");
 
 const app = express();
 let port = process.env.PORT as unknown;
@@ -7,12 +8,14 @@ if (port == null || port == "") {
   port = 8000;
 }
 app.listen(port, () => console.log(`Listening on port ${port}`));
+app.use(bodyParser.json());
 
 app.get("/", (req: Request, res: Response) => {
   res.status(200).send("This is just a Slack app. Nothing here!");
 });
 
 app.post("/slash", (req: Request, res: Response) => {
-  console.log("Request body: ", JSON.stringify(req.query));
+  console.log("Request query: ", JSON.stringify(req.query));
+  console.log("Request body: ", JSON.stringify(req.body));
   res.status(200).send("Got the slash command!");
 });
